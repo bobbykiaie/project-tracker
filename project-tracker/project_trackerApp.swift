7499157f649263905f9261899e02902576316e6a
Bobby Kiaie <bobbykiaie@gmail.com>
2023-12-27 15:08:37 -0800

    Initial Commit

---
+++ project-tracker/project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_trackerApp.swift
+//  project-tracker
+//
+//  Created by Babak Kiaie on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
